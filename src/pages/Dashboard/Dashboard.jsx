dc8f057d8b2db7fc549de491c53f22cf7b421c2b
SHIRISH <shirish.shrestha07@gmail.com>
2024-09-18 17:04:26 +0545

    change: admin dashboard redesigned

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,7 +4,13 @@ import {
   CompletedProjects,
   TotalProjects,
 } from "../../assets/icons/SvgIcons";
-import { BarChart, DoughnutChart, LineChart, Loader } from "../../components";
+import {
+  BarChart,
+  DoughnutChart,
+  LineChart,
+  Loader,
+  LogoutConfirmation,
+} from "../../components";
 import { useQuery } from "@tanstack/react-query";
 import {
   getProjects,
@@ -12,9 +18,27 @@ import {
 } from "../../api/Projects/ProjectsApiSlice";
 import EmptyData from "../../components/EmptyData/EmptyData";
 import useScrollRestoration from "../../hooks/useScrollRestoration";
+import { clientDashboard, curve, spiral, square } from "../../assets/images";
+import useLogout from "../../hooks/useLogout";
+import useAuth from "../../hooks/useAuth";
 
 export const Dashboard = () => {
   useScrollRestoration();
+
+  const { logout } = useLogout();
+
+  const { setLogoutConfirationShow, logoutConfirationShow, setAuth } =
+    useAuth();
+
+  const handleLogout = () => {
+    setAuth({});
+    localStorage.clear();
+    setLogoutConfirationShow(false);
+
+    logout(() => {
+      navigate("/login");
+    });
+  };
 
   const {
     isPending: projectPending,
@@ -121,10 +145,58 @@ export const Dashboard = () => {
 
   return (
     <section className="pt-[1rem]">
+      {logoutConfirationShow && (
+        <LogoutConfirmation
+          handleLogoutClick={handleLogout}
+          setLogoutConfirationShow={setLogoutConfirationShow}
+        />
+      )}
       {projectPending && <Loader />}
       {projectStatusPending && <Loader />}
       <div className="grid grid-cols-[1.3fr,0.7fr] gap-[1.2rem] w-full h-full">
-        <div className="w-full h-full">
+        <div className="w-full h-full flex flex-col gap-[1rem] justify-center">
+          <div className="w-full bg-white rounded-lg shadow-lg py-[1rem] px-[2rem] relative overflow-hidden">
+            <div className="flex items-center gap-[2rem] justify-between ">
+              <div className="relative z-10">
+                <h3 className="font-bold text-[1.8rem] text-center leading-[150%]">
+                  Welcome to the Admin Dashboard
+                </h3>
+              </div>
+              <figure className="w-[150px] relative z-10">
+                <img
+                  src={clientDashboard}
+                  alt="dashboard"
+                  className="object-cover"
+                />
+              </figure>
+              <img
+                src={square}
+                alt="square"
+                className="absolute left-[-5%] bottom-[80%]"
+              />
+              <img
+                src={square}
+                alt="square"
+                className="absolute left-[-12%] top-[80%]"
+              />
+              <img
+                src={curve}
+                alt="curve"
+                className="absolute rotate-90 right-[-10%] top-[40%]"
+              />
+              <img
+                src={curve}
+                alt="curve"
+                className="absolute rotate-90 right-[40%] top-[55%]"
+              />
+
+              <img
+                src={spiral}
+                alt="spiral"
+                className="absolute  right-[-1%] top-[-8%] h-[40px]"
+              />
+            </div>
+          </div>
           <div className="grid grid-cols-3 gap-[0.8rem] justify-center">
             <div className="p-[1rem] bg-primary w-full h-[167px] rounded-lg shadow-lg text-light">
               <div className="flex gap-[0.8rem] items-center">
@@ -177,16 +249,16 @@ export const Dashboard = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col justify-center items-center p-[1rem] mt-[1.5rem] bg-white w-full  rounded-lg shadow-lg">
+          {/* <div className="flex flex-col justify-center items-center p-[1rem] mt-[1.5rem] bg-white w-full  rounded-lg shadow-lg">
             <div className="mb-[1.8rem] text-center">
               <h4 className="font-bold text-[1.2rem] ">Material Usage</h4>
               <p className="text-[12px]">Last 1 year</p>
             </div>
             <LineChart lineData={lineData} datasets={lineDatasets} />
-          </div>
+          </div> */}
         </div>
         <div>
-          <div className=" h-fit p-[1rem] bg-white flex flex-col justify-center items-center shadow-lg rounded-lg">
+          {/* <div className=" h-fit p-[1rem] bg-white flex flex-col justify-center items-center shadow-lg rounded-lg">
             <h4 className="font-bold text-start">Team Performance</h4>
             <div className=" pb-[1.2rem] text-[12px] font-[500] pt-[0.2rem]">
               <p>Last 1 year</p>
@@ -199,8 +271,8 @@ export const Dashboard = () => {
                 // borderColors="rgba(24, 44, 58, 0.668)"
               />
             </div>
-          </div>
-          <div className="h-fit mt-[1.0rem] p-[1rem] bg-primary text-white flex flex-col justify-center items-center shadow-lg rounded-lg">
+          </div> */}
+          <div className="h-fit  p-[1rem] bg-primary text-white flex flex-col justify-center items-center shadow-lg rounded-lg">
             <h4 className="font-bold text-start pb-[1.2rem]">Project Status</h4>
 
             <div className="max-w-[340px] ">
