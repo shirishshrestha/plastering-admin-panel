f9a4458bdfbf9dd2ff76a22333e7a747004ea87f
SHIRISH <shirish.shrestha07@gmail.com>
2024-09-18 17:11:57 +0545

    add: navigate in dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   Calendar,
   CompletedProjects,
@@ -24,6 +24,8 @@ import useAuth from "../../hooks/useAuth";
 
 export const Dashboard = () => {
   useScrollRestoration();
+
+  const navigate = useNavigate();
 
   const { logout } = useLogout();
 
